78514accabb0716e7dd61e3dcc32a8efeb11753f
Oleksandr Tkachuk <sashagfu@gmail.com>
2020-04-14 14:09:55 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+<p align="center">
+<img src="https://res.cloudinary.com/dtfbvvkyp/image/upload/v1566331377/laravel-logolockup-cmyk-red.svg" height="100" style="float: left;">
+<img src="https://images.contentstack.io/v3/assets/bltefdd0b53724fa2ce/blt280217a63b82a734/5bbdaacf63ed239936a7dd56/elastic-logo.svg" height="60" style="float: left;">
+</p>
+
+## Elasticsearch in your Laravel App
+
+[Original article](https://madewithlove.com/how-to-integrate-elasticsearch-in-your-laravel-app-2019-edition/).
---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Articles\ArticlesRepository;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('articles.index', [
+        'articles' => App\Article::all(),
+    ]);
+});
+
+Route::get('search', function (ArticlesRepository $repository) {
+    $articles = $repository->search(request('q'));
+
+    return view('articles.index', [
+        'articles' => $articles,
+    ]);
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
